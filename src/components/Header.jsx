5d2eed9c2c5d38e2e854d168d46bd03224fb0b8a
Bibhushan <bibhus47@in.ibm.com>
2024-05-17 20:12:29 +0530

    Implemented Menu Wireframe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ export default function Header(){
 
     return (
         <div>
-            <div className='blue-box' style={{fontSize:12, padding:4}}>
+            <div className='blue-box' style={{fontSize:14, padding:4}}>
                 <p>The health and well-being of our patients and their health care team will always be our priority, 
                     so we follow the best practices for cleanliness.
                 </p>
             </div>
-            <NavBar/>
+            <NavBar />
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,43 @@
+import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import medifyLogo from '../assets/medify-logo.png';
 
 export default function NavBar(){
 
     return (
-        <div>
-            Nav bar goes here
+        <div className='menu'>
+            <div style={{display:'inherit'}}>
+                <img src={medifyLogo} style={{width:24, height:24, padding:'1rem 0.25rem'}}/>
+                <p style={{fontWeight:'bolder', color:'#2AA8FF'}}>Medify</p>
+            </div>            
+            <div style={{display:'inherit'}}>
+                <div className='menu-item'>Find Doctors</div>
+                <div className='menu-item'>Hospitals</div>
+                <div className='menu-item'>Medicines</div>
+                <div className='menu-item'>Surgeries</div>
+                <div className='menu-item'>Software for Providers</div>
+                <div className='menu-item'>My Bookings</div>
+            </div>
         </div>
     )
+    // return (
+    //     <Box >
+    //         <AppBar position="static">
+    //             <Toolbar>
+    //             <IconButton
+    //                 size="large"
+    //                 edge="start"
+    //                 color="inherit"
+    //                 aria-label="menu"
+    //                 sx={{ mr: 2 }}
+    //             >
+    //                 <medifyLogo />
+    //             </IconButton>
+    //             <Typography variant="h6" component="div" textTransform='none' sx={{ flexGrow: 1 }}>
+    //                 News
+    //             </Typography>
+    //             <Button color="inherit">Login</Button>
+    //             </Toolbar>
+    //         </AppBar>
+    //     </Box>
+    // )
 }
